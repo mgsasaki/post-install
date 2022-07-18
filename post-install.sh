c286ef270e1ffff014527ac354dba8dbc72bab5e
Marcello Gurgel Sasaki <mgsasaki@users.noreply.github.com>
2022-07-18 18:54:46 -0300

    Update docker installation instructions

--- post-install.sh
+++ post-install.sh
@@ -36,47 +36,30 @@ sudo apt-get install sublime-text
 ##########
 # Install packages to allow apt to use a repository over HTTPS:
 sudo apt-get install \
-    apt-transport-https \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
 
 # Add Docker’s official GPG key:
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 # Use the following command to set up the stable repository. To add the nightly or test repository, add the word nightly or test (or both) after the word stable in the commands below.
 echo \
-    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update the apt package index, and install the latest version of Docker Engine and containerd
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-echo -e '\n=> Removing Sudo requirement Docker' 
+# Receiving a GPG error when running apt-get update, try uncommenting the line bellow and try again.
+#sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo -e '\n=> Removing Sudo requirement Docker'
+#sudo groupadd docker
 sudo usermod -aG docker ${USER}
-
-##########
-# Install docker-compose (https://docs.docker.com/compose/install)
-##########
-# Download the current stable release of Docker Compose:
-docker-compose-version = "1.29.1"
-sudo curl \
-    -L "https://github.com/docker/compose/releases/download/${docker-compose-version}/docker-compose-$(uname -s)-$(uname -m)" \
-    -o /usr/local/bin/docker-compose
-
-# Apply executable permissions to the binary:
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Install line completion (https://docs.docker.com/compose/completion)
-# Bash:
-sudo curl \
-    -L "https://raw.githubusercontent.com/docker/compose/${docker-compose-version}/contrib/completion/bash/docker-compose" \
-    -o /etc/bash_completion.d/docker-compose
-
-# Reload your setting in your current terminal:
-source ~/.bashrc
 
 
 # TODO: Install asdf-vm (https://github.com/asdf-vm/asdf)
