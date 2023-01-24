55eb7686ff4c93a18373a84a8f0a13ac118ed21b
Marcello Gurgel Sasaki <mgsasaki@users.noreply.github.com>
2023-01-24 16:10:33 -0300

    Update run-ci-locally.sh

--- scripts/run-ci-locally.sh
+++ scripts/run-ci-locally.sh
@@ -2,6 +2,9 @@
 
 REPOS_DIR=${PWD}/..
 PROJECT_NAME=${PWD##*/}
+CURRENT_BRANCH=$(git branch --show-current)
+CURRENT_BRANCH_SLUG=${CURRENT_BRANCH//\//-}
+CURRENT_COMMIT_SHORT=$(git rev-parse --short HEAD)
 
 printf '\x1B[1;36mRunning job "%q" for project "%q"...\x1B[0m\n' "$1" "$PROJECT_NAME"
 
@@ -12,5 +15,5 @@ gitlab-runner exec docker \
 	--cache-dir /tmp/gitlab-runner-cache \
         --env CI_MBED_PACKAGE=framework-mbed \
 	--env CI_API_V4_URL=https://gitlab.com/api/v4 \
-	--env CI_COMMIT_REF_SLUG=$(git rev-parse --short HEAD) \
+	--env CI_COMMIT_REF_SLUG=$CURRENT_BRANCH_SLUG \
 	$1
