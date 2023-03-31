0f358467203353052287a2edd4a63c4fb6857438
Marcello Gurgel Sasaki <marcello.sasaki@gmail.com>
2023-03-31 11:53:47 -0300

    feat(git): add alias to diff whole dir with meld

--- post-install.sh
+++ post-install.sh
@@ -30,10 +30,15 @@ echo "Install asdf by putting the following lines on ~/.bashrc"
 echo '. $HOME/.asdf/asdf.sh'
 echo '. $HOME/.asdf/completions/asdf.bash'
 
+if [ ! -d "$HOME/.local/bin" ]; then
+    mkdir -p $HOME/.local/bin
+fi
+
 # TODO: dotfiles (https://github.com/skwp/dotfiles)
 
 ##########
 # Install sublime text editor (https://www.sublimetext.com/docs/3/linux_repositories.html)
+# TODO: use snap?
 ##########
 # Install the GPG key:
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
