fc333427fd2db32dbb47d4dabd0d6534fe3561dc
Marcello Gurgel Sasaki <marcello.sasaki@gmail.com>
2023-12-08 11:40:55 -0300

    feat: update information about tk dark theme instalation

--- install_tk_dark_theme.sh
+++ install_tk_dark_theme.sh
@@ -3,6 +3,9 @@
 # Go to https://wiki.tcl-lang.org/page/List+of+ttk+Themes and download awdark theme
 # or install with apt
 # sudo apt install tcl-awthemes
+# fix missing awbreezedark.tcl on /usr/share/tcltk/awthemes/
+# download tksvg from https://wiki.tcl-lang.org/page/tksvg
+# put tksvg on /usr/share/tcltk/
 
 $ mkdir ~/.local/share/tk-themes
 $ mkdir ~/.local/share/tk-themes/awthemes
