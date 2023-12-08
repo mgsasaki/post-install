e4a1d366feec9abcaf774721c64f253129062b74
Marcello Gurgel Sasaki <msasaki@neo-classic.local>
2023-12-07 23:51:14 -0300

    feat: instruction to install awdark theme

---
+++ install_tk_dark_theme.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Go to https://wiki.tcl-lang.org/page/List+of+ttk+Themes and download awdark theme
+# or install with apt
+# sudo apt install tcl-awthemes
+
+$ mkdir ~/.local/share/tk-themes
+$ mkdir ~/.local/share/tk-themes/awthemes
+$ mv ~/Downloads/awthemes.tcl ~/.local/share/tk-themes/awthemes/
+
+$ nano ~/.local/share/tk-themes/awthemes/pkgIndex.tcl
+
+package ifneeded ttk::theme::awdark 2.3 [list source [file join $dir awthemes.tcl]]
+
+$ nano ~/.profile
+
+# TK Themes
+export TCLLIBPATH=$HOME/.local/share/tk-themes
+
+$ nano ~/.Xresources
+
+*TkTheme: awdark
